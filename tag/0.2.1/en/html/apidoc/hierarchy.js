8a495e2f3c6d396b89be93b5fb3621a6c809fc93
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-06-10 23:27:22 +0000

    Deploy Erebot/erebot.github.io to github.com/Erebot/erebot.github.io.git:build-Erebot/Module_WatchList

---
+++ tag/0.2.1/en/html/apidoc/hierarchy.js
@@ -0,0 +1,9 @@
+var hierarchy =
+[
+    [ "Base", null, [
+      [ "Erebot\\Module\\WatchList", "classErebot_1_1Module_1_1WatchList.html", null ]
+    ] ],
+    [ "HelpEnabled", null, [
+      [ "Erebot\\Module\\WatchList", "classErebot_1_1Module_1_1WatchList.html", null ]
+    ] ]
+];
